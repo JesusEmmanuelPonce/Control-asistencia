00332fc931cafa26b7027872d631b90def59f7ba
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-30 12:51:58 -0600

    form file moved to other folder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import Header from './components/header/header';
-import FormAssistance from './components/formAssistance';
+import FormAssistance from './components/form/formAssistance';
+import Logo from './components/logo/logo';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <Logo/>
       <FormAssistance/>
     </div>
   );
