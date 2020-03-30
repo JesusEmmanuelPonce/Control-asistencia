c5d6aa79b5dbe5220cb2bc0ccf77dd5c22152344
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-30 12:25:52 -0600

    header and assistance form components

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+    return(
+        <div className="container-header-pb">
+            
+        </div>
+    )
+}
+
+export default Header;
