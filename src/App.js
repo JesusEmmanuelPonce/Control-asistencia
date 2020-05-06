8efb0bc4db2c424ffd65b157394e9fe95d54d4e1
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-05-06 12:49:12 -0500

    small changed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import Header from './components/header/header';
+import Clock from './components/clock/Clock';
 import FormAssistance from './components/form/formAssistance';
-import Logo from './components/logo/logo';
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
-        <Logo/>
+        <Clock/>
       <FormAssistance/>
     </div>
   );
