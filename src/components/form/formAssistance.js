02bd08ef1904ba959a79133b8664e02a15ce4ca1
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-05-08 16:22:00 -0500

    send post parameter

--- src/components/form/formAssistance.js
+++ src/components/form/formAssistance.js
@@ -12,6 +12,27 @@ const FormAssistance = ({history}) => {
     const assistanceSumit = async e => {
         e.preventDefault()
 
+        const event = new Date();
+        let h = event.getHours();
+        let m = event.getMinutes();
+        let s = event.getSeconds();
+
+        let day = event.getDate();
+        let month = event.getMonth() + 1;
+        const year = event.getFullYear();
+        
+            if(s < 10) {
+                s = '0' + s 
+            }
+            if(m < 10) {
+                m = '0' + m
+            }
+            if(h < 10) {
+                h = '0' + h
+            }
+
+        const hour = `${h}:${m}:${s}`;
+
         if(!id){
             setError(true);
             Swal.fire('Escriba su ID')
@@ -21,6 +42,10 @@ const FormAssistance = ({history}) => {
         setError(false)
         
         await axios.post('https://assistances-back.herokuapp.com/api/entry', {
+            day,
+			month,
+			year,
+			hour,
             id
         },
             Swal.fire(
